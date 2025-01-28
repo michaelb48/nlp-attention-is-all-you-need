f4547976e06bc3f5aedaa95ebc5e8cc6594fb029
Michael <e11775807@student.tuwien.ac.at>
2025-01-28 22:17:14 +0000

    fix: fix compile error in optimizer

--- Optimizer.py
+++ Optimizer.py
@@ -1,9 +1,8 @@
 import torch
-from torch.optim import Adam
 
 class CustomOptim:
-    def __init__(self, model, lr=1e-4, beta1=0.9, beta2=0.98, eps = 1e-9, d_model=512, n_warmup_steps=4000, lr_factor=1):
-        self.optimizer = Adam(model.parameters(), lr=lr, betas=(beta1, beta2), eps=eps),
+    def __init__(self, optimizer, lr=1e-4, beta1=0.9, beta2=0.98, eps = 1e-9, d_model=512, n_warmup_steps=4000, lr_factor=1):
+        self.optimizer = optimizer
         self.eps = eps
         self.lr_factor = lr_factor
         self.d_model = d_model
@@ -13,9 +12,12 @@ class CustomOptim:
     def zero_grad(self):
         self.optimizer.zero_grad()
 
+    def get_lr(self):
+        return self.lr_factor * ((self.d_model ** -0.5) * min(self.n_steps ** -0.5, self.n_steps * (self.n_warmup_steps ** -1.5)))
+        
     def lr_step(self):
         # calculate according to formula in paper
-        lr = lr_factor * ((d_model ** -0.5) * min(n_steps ** -0.5, n_steps * (n_warmup_steps ** -1.5)))
+        lr = self.get_lr()
 
         # update the lr for every param in the optimizer
         for param_group in self.optimizer.param_groups:
