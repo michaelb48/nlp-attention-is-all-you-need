8072387556ad5a23e1538127c0482646b2dd7817
Janick Boehm <janick255@gmail.com>
2025-01-30 12:33:51 +0000

    Added comments

--- PositionalEncoding.py
+++ PositionalEncoding.py
@@ -6,18 +6,19 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
         super().__init__()
 
-        # Create positional encoding matrix
-        pe = torch.zeros(max_len, d_model)
+        empty_positions = torch.zeros(max_len, d_model)
+        
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        
         # 10 000 was used in the paper
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
 
-        # Calculate positional encodings
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+        # Calculates sin for even positions i in d_model and cos for odd
+        empty_positions[:, 0::2] = torch.sin(position * div_term)
+        empty_positions[:, 1::2] = torch.cos(position * div_term)
 
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
+        pos_encoding = empty_positions.unsqueeze(0)
+        self.register_buffer('pos_encoding', pos_encoding)
 
     def forward(self, x):
         return x + self.pe[:, :x.size(1)]
